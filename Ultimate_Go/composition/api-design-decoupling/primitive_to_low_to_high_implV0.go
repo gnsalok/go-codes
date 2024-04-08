bee10e3db2d6a7f457325a8c5892eb444a3f8aa9
Alok Tripathi <alok.k.tripathi@aexp.com>
2024-04-08 14:08:16 +0530

    exe: api decoupling design

---
+++ Ultimate_Go/composition/api-design-decoupling/primitive_to_low_to_high_implV0.go
@@ -0,0 +1,134 @@
+// Sample program demonstrating struct composition.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
+)
+
+/*
+	- From Primitive layer ->  Low layer ->  High Level Implementation
+
+*/
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// =============================================================================
+
+// Data is the structure of the data we are copying.
+type Data struct {
+	Line string
+}
+
+// Primitive Layer =============================================================================
+
+// Xenia is a system we need to pull data from.
+type Xenia struct {
+	Host    string
+	Timeout time.Duration
+}
+
+// Pull knows how to pull data out of Xenia.
+func (*Xenia) Pull(d *Data) error {
+	switch rand.Intn(10) {
+	case 1, 9:
+		return io.EOF
+
+	case 5:
+		return errors.New("error reading data from Xenia")
+
+	default:
+		d.Line = "Data"
+		fmt.Println("In:", d.Line)
+		return nil
+	}
+}
+
+// Pillar is a system we need to store data into.
+type Pillar struct {
+	Host    string
+	Timeout time.Duration
+}
+
+// Store knows how to store data into Pillar.
+func (*Pillar) Store(d *Data) error {
+	fmt.Println("Out:", d.Line)
+	return nil
+}
+
+// Low Layer =============================================================================
+
+// System wraps Xenia and Pillar together into a single system.
+type System struct {
+	Xenia
+	Pillar
+}
+
+// =============================================================================
+
+// pull knows how to pull bulks of data from Xenia.
+func pull(x *Xenia, data []Data) (int, error) {
+	for i := range data {
+		if err := x.Pull(&data[i]); err != nil {
+			return i, err
+		}
+	}
+
+	return len(data), nil
+}
+
+// store knows how to store bulks of data into Pillar.
+func store(p *Pillar, data []Data) (int, error) {
+	for i := range data {
+		if err := p.Store(&data[i]); err != nil {
+			return i, err
+		}
+	}
+
+	return len(data), nil
+}
+
+// High Layer ----------------------
+
+// Copy knows how to pull and store data from the System.
+func Copy(sys *System, batch int) error {
+	data := make([]Data, batch)
+
+	for {
+		i, err := pull(&sys.Xenia, data)
+		if i > 0 {
+			if _, err := store(&sys.Pillar, data[:i]); err != nil {
+				return err
+			}
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+}
+
+// =============================================================================
+
+func main() {
+	sys := System{
+		Xenia: Xenia{
+			Host:    "localhost:8000",
+			Timeout: time.Second,
+		},
+		Pillar: Pillar{
+			Host:    "localhost:9000",
+			Timeout: time.Second,
+		},
+	}
+
+	if err := Copy(&sys, 3); err != io.EOF {
+		fmt.Println(err)
+	}
+}
