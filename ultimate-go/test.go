3b3dfa89ba351419c378952c4a5cee6b8903263a
Alok Tripathi <gnsalok@gmail.com>
2025-09-06 17:56:44 +0530

    refactor: refactoring projects directories

---
+++ ultimate-go/test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	// write channel to add even and odd numbers and read channel to read them
+	evenCh := make(chan int)
+	oddCh := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// Goroutine to send even numbers
+	go func() {
+		defer wg.Done()
+		for i := 0; i <= 10; i++ {
+			if i%2 == 0 {
+				evenCh <- i
+			}
+		}
+		close(evenCh)
+	}()
+
+	// Goroutine to send odd numbers
+	go func() {
+		defer wg.Done()
+		for i := 0; i <= 10; i++ {
+			if i%2 != 0 {
+				oddCh <- i
+			}
+		}
+		close(oddCh)
+	}()
+
+	// Goroutine to read even numbers
+
+	for num := range evenCh {
+		fmt.Println("Even:", num)
+	}
+
+	// Goroutine to read odd numbers
+
+	for num := range oddCh {
+		fmt.Println("Odd:", num)
+	}
+
+	wg.Wait()
+}
